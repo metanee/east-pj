ef1a6e772cc7e51139dde742999e3640643ba6c1
MivJi <metanee.sae@gmail.com>
2018-09-18 17:42:21 +0700

    Add Employee List

--- src/main/java/com/east/controller/EmployeeController.java
+++ src/main/java/com/east/controller/EmployeeController.java
@@ -2,11 +2,11 @@ package com.east.controller;
 
 import java.security.Principal;
 import java.util.HashMap;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -39,15 +39,26 @@ public class EmployeeController  {
 			Principal principal) {
 		Employee employee = new Employee();
 		int id =  (Integer) mapper.get("userId");
-		System.out.println(id);
+		//System.out.println(id);
 		Company company = companyService.findByUsername(principal.getName());
 		User user = userService.findById(Long.valueOf(id));
-		System.out.println(user.getEmail());
+		//System.out.println(user.getEmail());
 		companyService.updateUserPaymentInfo(employee, company ,user );
 		
 		
 		return new ResponseEntity("Payment Added(Updated) Successfully!", HttpStatus.OK);
 	}
 	
+	@RequestMapping("/getEmployeeList")
+	public List<Employee> getUserPaymentList(
+			Principal principal
+			){
+		Company company = companyService.findByUsername(principal.getName());
+		System.out.println(company.getEmail());
+		List<Employee> userPaymentList = company.getEmployeeList();
+		System.out.println(userPaymentList);
+		
+		return userPaymentList;
+	}
 	
 }
